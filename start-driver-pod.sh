d0443e864df653e91a163eab9a2b137ba35e2ef3
Chris Olen <chrisolen1@gmail.com>
2020-03-04 21:53:54 -0600

    updates to spark driver

--- start-driver-pod.sh
+++ start-driver-pod.sh
@@ -1,2 +1,3 @@
-kubectl run $DRIVER_NAME --rm=true -ti -n $SPARK_NAMESPACE --image=$DRIVER_IMAGE --serviceaccount=$DRIVER_POD_SA
+kubectl run $DRIVER_NAME --rm=true -ti -n $SPARK_NAMESPACE --image=$DRIVER_IMAGE --serviceaccount=$DRIVER_POD_SA \
+--requests=cpu=2,memory=2G
 	
